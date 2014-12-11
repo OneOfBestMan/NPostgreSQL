47d6ffce5351d7a4a1652d52577a28119ad1e7d6
Riaan <riaan@definitive-software.com>
2014-12-11 13:15:51 +0200

    Add building blocks

---
+++ src/API/PgCtrl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API
+{
+    internal class PgCtrl
+    {
+        #region Public
+        // Initialize
+
+        // Start
+
+        // Stop
+
+        // Restart
+
+        // Reload
+
+        // Status
+
+        // Kill
+
+        // Register
+
+        // UnRegister
+
+        #endregion
+
+        #region Private
+        
+        #endregion
+    }
+}
