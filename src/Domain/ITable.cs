3482e8e9b585dcb529b604bd5891a2f465666664
Riaan van der Westhuizen <rvdwesthuizen@compuscan.co.za>
2018-05-08 15:00:53 +0200

    Start new projects

---
+++ src/Domain/ITable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    /// <summary>
+    /// Represents a Table
+    /// </summary>
+    public interface ITable
+    {
+    }
+}
